df8c6766017bc725c7bb2c0bb8dc3ec47af04536
MatiPino <mati.pinos@duoucuc.cl>
2021-10-04 22:09:10 -0300

    Se creo la interfaz faltante de sugerencias y rankings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { Component, NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { CategoriasComponent } from 'src/pages/categorias/categorias.component';
 import { InicioComponent } from 'src/pages/inicio/inicio.component';
+import { RankingsComponent } from 'src/pages/rankings/rankings.component';
 import { RecuperarComponent } from 'src/pages/recuperar/recuperar.component';
 import { RegistrarComponent } from 'src/pages/registrar/registrar.component';
 import { SesionComponent } from 'src/pages/sesion/sesion.component';
+import { SugerenciasComponent } from 'src/pages/sugerencias/sugerencias.component';
 
 const routes: Routes = [
 {
@@ -26,6 +28,14 @@ const routes: Routes = [
 {
   path: 'categorias',
   component: CategoriasComponent
+},
+{
+  path: 'sugerencias',
+  component: SugerenciasComponent
+},
+{
+  path: 'rankings',
+  component: RankingsComponent
 },  
 ];
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { CategoriasComponent } from 'src/pages/categorias/categorias.component';
 import { InicioComponent } from 'src/pages/inicio/inicio.component';
+import { RankingsComponent } from 'src/pages/rankings/rankings.component';
 import { RecuperarComponent } from 'src/pages/recuperar/recuperar.component';
 import { RegistrarComponent } from 'src/pages/registrar/registrar.component';
 import { SesionComponent } from 'src/pages/sesion/sesion.component';
+import { SugerenciasComponent } from 'src/pages/sugerencias/sugerencias.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +21,9 @@ import { AppComponent } from './app.component';
     InicioComponent, 
     RecuperarComponent, 
     RegistrarComponent, 
-    CategoriasComponent],
+    CategoriasComponent,
+    SugerenciasComponent,
+    RankingsComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
